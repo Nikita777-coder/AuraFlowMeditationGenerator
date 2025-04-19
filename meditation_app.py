198ab6743fa983c0fd30b50d1dc691aefd1a6642
Nikita777-coder <61533809+Nikita777-coder@users.noreply.github.com>
2025-04-19 20:55:25 +0300

    add redis and async cleanup

--- meditation_app.py
+++ meditation_app.py
@@ -21,6 +21,7 @@ load_dotenv()
 
 REDIS_URL = os.getenv("REDIS_URL")
 redis_client = redis.from_url(REDIS_URL, decode_responses=True)
+print(redis_client)
 
 logging.basicConfig(filename="app.log", level=logging.ERROR)
 
